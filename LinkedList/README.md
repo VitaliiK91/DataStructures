1e46dcca50d357702a4f8d385943792c77aa0ae6
vitalii kalinin <vitaliik@civicresource.com>
2017-04-24 20:22:36 -0700

    add readme

---
+++ LinkedList/README.md
@@ -0,0 +1,6 @@
+#H1 Data Structures in JS #H1
+
+#H2 To Do
+ - [x] Linked List
+ - [ ] Binary Tree
+ - [ ] Heap
--- LinkedList/example.js
+++ LinkedList/example.js
@@ -4,4 +4,10 @@ const tree = new Tree();
 for (let i = 0; i < 10; i++) {
 	tree.add(i);
 }
+
+tree.remove(3);
 console.log(JSON.stringify(tree));
+
+tree.reverse();
+console.log(JSON.stringify(tree));
+
